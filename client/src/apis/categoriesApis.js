58e85bcbf2d1d1d854fc35e77203330742a16c5c
Joseph-Essa <josepheissa0@gmail.com>
2024-05-08 00:16:19 +0300

    no changes

--- client/src/apis/categoriesApis.js
+++ client/src/apis/categoriesApis.js
@@ -5,18 +5,15 @@ const CategoryApi = axios.create({
     baseURL: 'http://127.0.0.1:8000',
 });
 
-// export const getUsers = () => userApi.get('/');
-// export const deleteUser = id => userApi.delete(`/${id}`);
-// export const isAdmin = () => userApi.post(data)
-
 export const getAllcategories = () => CategoryApi.get('/categories/')
 export const Addcategories = (name) => CategoryApi.post('/categories/',{ name })
 export const Deletecategory = (id) => CategoryApi.delete(`/categories/${id}`)
-
+export const getCategoryByID = (id) => CategoryApi.get(`/categories/id ${id}`)
 const apis = {
     getAllcategories,
     Addcategories,
-    Deletecategory
+    Deletecategory,
+    getCategoryByID
 }
 
 export default apis;
