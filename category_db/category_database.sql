cda8bdbf85b4f31ebab7364e1f9c0964360476e8
Joseph-Essa <josepheissa0@gmail.com>
2024-05-07 03:17:01 +0300

    no_change

--- category_db/category_database.sql
+++ category_db/category_database.sql
@@ -8,13 +8,13 @@ CREATE TABLE IF NOT EXISTS categories (
     name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS products (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    price INT  NOT NULL,
-    category_id INT,
-    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
-);
+-- CREATE TABLE IF NOT EXISTS products (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(255) NOT NULL,
+--     price INT  NOT NULL,
+--     category_id INT,
+--     FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+-- );
 
 
 --Inserting data into the categories table
@@ -24,11 +24,11 @@ INSERT INTO categories (name) VALUES
     ('Books');
 
 -- Inserting data into the products table
-INSERT INTO products (name, price, category_id) VALUES
-    ('Smartphone', 499.99, 1),
-    ('Laptop', 899.99, 1),
-    ('T-shirt', 19.99, 2),
-    ('Jeans', 39.99, 2),
-    ('Novel', 12.99, 3),
-    ('Textbook', 79.99, 3);
+-- INSERT INTO products (name, price, category_id) VALUES
+--     ('Smartphone', 499.99, 1),
+--     ('Laptop', 899.99, 1),
+--     ('T-shirt', 19.99, 2),
+--     ('Jeans', 39.99, 2),
+--     ('Novel', 12.99, 3),
+--     ('Textbook', 79.99, 3);
 
