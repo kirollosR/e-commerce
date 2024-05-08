dce636546ca57f0fea1c8b220ba8962dda187f50
YousefEmiel <youssefemielio@gmail.com>
2024-05-08 03:32:05 +0300

    updates categoriesApis

--- client/src/apis/categoriesApis.js
+++ client/src/apis/categoriesApis.js
@@ -2,7 +2,7 @@ import { data } from 'autoprefixer';
 import axios from 'axios';
 
 const CategoryApi = axios.create({
-    baseURL: 'http://127.0.0.1:8000',
+    baseURL: 'http://127.0.0.1:5003',
 });
 
 // export const getUsers = () => userApi.get('/');
@@ -12,11 +12,13 @@ const CategoryApi = axios.create({
 export const getAllcategories = () => CategoryApi.get('/categories/')
 export const Addcategories = (name) => CategoryApi.post('/categories/',{ name })
 export const Deletecategory = (id) => CategoryApi.delete(`/categories/${id}`)
+export const getCategoryID = (id) => CategoryApi.get(`/categories/id${id}`)
 
 const apis = {
     getAllcategories,
     Addcategories,
-    Deletecategory
+    Deletecategory,
+    getCategoryID
 }
 
 export default apis;
